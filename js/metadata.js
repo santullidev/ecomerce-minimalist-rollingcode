48422bbe77b8ba5b00261e81c1564d92a6826bf7
Colchi <Julio.Colchi@Sovos.com>
2023-07-20 19:00:34 -0300

    Moving array of products into a separate file

---
+++ js/metadata.js
@@ -0,0 +1,206 @@
+const productos= [
+    //abrigos
+    {
+       id:"abrigo-01",
+       titulo: "abrigo-01",
+       imagen: "./img/abrigos/01.jpg",
+       categoria:{
+          nombre:"Abrigos",
+          id:"abrigos"
+       },
+       precio: 1000,
+    },
+    {
+       id:"abrigo-02",
+       titulo: "abrigo-02",
+       imagen: "./img/abrigos/02.jpg",
+       categoria:{
+          nombre:"Abrigos",
+          id:"abrigos"
+       },
+       precio: 1000,
+    },
+    {
+       id:"abrigo-03",
+       titulo: "abrigo-03",
+       imagen: "./img/abrigos/03.jpg",
+       categoria:{
+          nombre:"Abrigos",
+          id:"abrigos"
+       },
+       precio: 1000,
+    },
+    {
+       id:"abrigo-04",
+       titulo: "abrigo-04",
+       imagen: "./img/abrigos/04.jpg",
+       categoria:{
+          nombre:"Abrigos",
+          id:"abrigos"
+       },
+       precio: 1000,
+    },
+    {
+       id:"abrigo-05",
+       titulo: "abrigo-05",
+       imagen: "./img/abrigos/05.jpg",
+       categoria:{
+          nombre:"Abrigos",
+          id:"abrigos"
+       },
+       precio: 1000,
+    },
+    {
+       id:"abrigo-06",
+       titulo: "abrigo-06",
+       imagen: "./img/abrigos/06.jpg",
+       categoria:{
+          nombre:"Abrigos",
+          id:"abrigos"
+       },
+       precio: 1000,
+    },
+    //pantalones
+    {
+       id:"pantalones-01",
+       titulo: "pantalones-01",
+       imagen: "./img/pantalones/01.jpg",
+       categoria:{
+          nombre:"Pantalones",
+          id:"pantalones"
+       },
+       precio: 1000,
+    },
+    {
+       id:"pantalones-02",
+       titulo: "pantalones-02",
+       imagen: "./img/pantalones/02.jpg",
+       categoria:{
+          nombre:"Pantalones",
+          id:"pantalones"
+       },
+       precio: 1000,
+    },
+    {
+       id:"pantalones-03",
+       titulo: "pantalones-03",
+       imagen: "./img/pantalones/03.jpg",
+       categoria:{
+          nombre:"Pantalones",
+          id:"pantalones"
+       },
+       precio: 1000,
+    },
+    {
+       id:"pantalones-04",
+       titulo: "pantalones-04",
+       imagen: "./img/pantalones/04.jpg",
+       categoria:{
+          nombre:"Pantalones",
+          id:"pantalones"
+       },
+       precio: 1000,
+    },
+    {
+       id:"pantalones-05",
+       titulo: "pantalones-05",
+       imagen: "./img/pantalones/05.jpg",
+       categoria:{
+          nombre:"Pantalones",
+          id:"pantalones"
+       },
+       precio: 1000,
+    },
+    {
+       id:"pantalones-06",
+       titulo: "pantalones-06",
+       imagen: "./img/pantalones/06.jpg",
+       categoria:{
+          nombre:"Pantalones",
+          id:"pantalones"
+       },
+       precio: 1000,
+    },
+    //remeras
+    {
+       id:"remeras-01",
+       titulo: "remeras-01",
+       imagen: "./img/remeras/01.jpg",
+       categoria:{
+          nombre:"Remeras",
+          id:"remeras"
+       },
+       precio: 1000,
+    },
+    {
+       id:"remeras-02",
+       titulo: "remeras-02",
+       imagen: "./img/remeras/02.jpg",
+       categoria:{
+          nombre:"Remeras",
+          id:"remeras"
+       },
+       precio: 1000,
+    },
+    {
+       id:"remeras-03",
+       titulo: "remeras-03",
+       imagen: "./img/remeras/03.jpg",
+       categoria:{
+          nombre:"Remeras",
+          id:"remeras"
+       },
+       precio: 1000,
+    },
+    {
+       id:"remeras-04",
+       titulo: "remeras-04",
+       imagen: "./img/remeras/04.jpg",
+       categoria:{
+          nombre:"Remeras",
+          id:"remeras"
+       },
+       precio: 1000,
+    },
+    {
+       id:"remeras-05",
+       titulo: "remeras-05",
+       imagen: "./img/remeras/05.jpg",
+       categoria:{
+          nombre:"Remeras",
+          id:"remeras"
+       },
+       precio: 1000,
+    },
+    {
+       id:"remeras-06",
+       titulo: "remeras-06",
+       imagen: "./img/remeras/06.jpg",
+       categoria:{
+          nombre:"Remeras",
+          id:"remeras"
+       },
+       precio: 1000,
+    },
+    {
+       id:"remeras-07",
+       titulo: "remeras-07",
+       imagen: "./img/remeras/07.jpg",
+       categoria:{
+          nombre:"Remeras",
+          id:"remeras"
+       },
+       precio: 1000,
+    },
+    {
+       id:"remeras-08",
+       titulo: "remeras-08",
+       imagen: "./img/remeras/08.jpg",
+       categoria:{
+          nombre:"Remeras",
+          id:"remeras"
+       },
+       precio: 1000,
+    },
+ ]; 
+ 
