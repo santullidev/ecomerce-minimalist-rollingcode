6903644a8741f1515564904d22c4bfadc10e0b73
Colchi <Julio.Colchi@Sovos.com>
2023-07-24 19:12:37 -0300

    Adding responsive behaviour

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@ const tituloPrincipal= document.querySelector("#titulo-principal");
 let botonesComprar = document.querySelectorAll(".btn-comprar");
 const cantidadItems = document.querySelector("#cantidad-items");
 
-
-
-
 function cargarProductos(productosElegidos){
    contenedorProductos.innerHTML= ""; // primero limpiamos el contenedor antes de cargarlo
    
