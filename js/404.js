3c1bad4c16a35ceed1afa20b54e60807ca32dbbc
unknown <lucas.miguel.albarado24@gmail.com>
2023-07-26 21:23:32 -0300

    se separo el nav-footer

--- js/404.js
+++ js/404.js
@@ -1,9 +1,15 @@
 const btn404 = document.querySelector('#btn404');
 
 btn404.addEventListener('mouseover', function () {
-    const randomX = parseInt(Math.random()*100);
-    const randomY = parseInt(Math.random()*100);
-    btn404.style.setProperty('top', randomY+'%');
-    btn404.style.setProperty('left', randomX+'%');
-    btn404.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
-})  
+    const minX = 10;
+    const maxX = 70;
+    const minY = 10;
+    const maxY = 70;
+
+    const randomX = Math.random() * (maxX - minX) + minX;
+    const randomY = Math.random() * (maxY - minY) + minY;
+
+    btn404.style.setProperty('top', randomY + '%');
+    btn404.style.setProperty('left', randomX + '%');
+    btn404.style.setProperty('transform', `translate(-${randomX}%, -${randomY}%)`);
+})
